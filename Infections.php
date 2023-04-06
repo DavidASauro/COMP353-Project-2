6a46e13c8f9eaa40d7d6b830c2809c9f8c8ab5bb
David Sauro <DavidSau43@Gmail.com>
2023-04-06 13:07:41 -0400

    All tables are now visible through nav bar

---
+++ Infections.php
@@ -0,0 +1,83 @@
+<?php
+include 'Connect.php'
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Our Database [PLACEHOLDER]</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
+</head>
+
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Hospital System</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="Employees.php">Employees</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Facilities.php">Facilities</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Vaccines.php">Vaccines</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Infections.php">Infections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Email.php">Email</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Schedule.php">Schedule</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="WorksAt.php">WorksAt</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Arranges.php">Arranges</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Sends.php">Sends</a>
+      </li>
+  </div>
+</nav>
+
+
+<?php 
+
+$tablename = 'Infections';
+$query = 'SHOW COLUMNS FROM '. $tablename; 
+$column_names = mysqli_query($conn, $query);
+echo "<table class =\"table\">";
+echo "<tr>";
+while($row = mysqli_fetch_assoc($column_names)) {
+	echo '<th>' . $row['Field'] . '</th>';
+}
+
+echo '<th> Delete </th>';
+
+
+ $query = 'SELECT * FROM '. $tablename;
+ $table_data = mysqli_query($conn, $query);
+ while ($row = mysqli_fetch_assoc($table_data)) {
+    echo "<tr>";
+    foreach ($row as $value) {
+        echo "<td>" . $value . "</td>";
+    }
+
+	echo "<td> <button type ='submit' class= \"btn btn-outline-danger\" name ='delete' value =''> Delete </button> </td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
+
+</body>
+</html>
